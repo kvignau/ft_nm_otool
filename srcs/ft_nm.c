a357f94c4612d899ea1b72585f5a3c9c92f20690
Kevin VIGNAU <kvignau@e2r3p9.42.fr>
2018-03-28 12:24:30 +0200

    fix(): begin ft_nm

--- srcs/ft_nm.c
+++ srcs/ft_nm.c
@@ -12,14 +12,50 @@
 
 #include "ft_nm.h"
 
+void				print_output(int nsyms, int symoff, int stroff, void *ptr)
+{
+	int				i;
+	char			*stringtable;
+	struct nlist_64	*array;
+
+	i = 0;
+	array = ptr + symoff;
+	stringtable = ptr + stroff;
+	while (i < nsyms)
+	{
+		ft_printf("%s\n", stringtable + array[i].n_un.n_strx);
+		i++;
+	}
+}
+
 void				ft_handle_64(void *ptr)
 {
+	int						ncmds;
+	int						i;
+	struct mach_header_64	*header;
+	struct load_command		*lc;
+	struct symtab_command	*sym;
 
+	i = 0;
+	header = (struct mach_header_64 *)ptr;
+	ncmds = header->ncmds;
+	lc = ptr + sizeof(*header);
+	while (i < ncmds)
+	{
+		if (lc->cmd == LC_SYMTAB)
+		{
+			sym = (struct symtab_command *)lc;
+			print_output(sym->nsyms, sym->symoff, sym->stroff, ptr);
+			break;
+		}
+		lc = (void *)lc + lc->cmdsize;
+		i++;
+	}
 }
 
 void				ft_nm(void *ptr)
 {
-	int				magic_number;
+	unsigned int	magic_number;
 
 	magic_number = *(int *)ptr;
 	if (magic_number == MH_MAGIC_64)
@@ -41,7 +77,7 @@ int					main(int ac, char **av)
 	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
 		ft_errors("mmap error");
 	ft_nm(ptr);
-	if (munmap(ptr, buff.st_size) < 0)
+	if (munmap(ptr, buf.st_size) < 0)
 		ft_errors("munmap (free) error");
 	return (EXIT_SUCCESS);
 }
